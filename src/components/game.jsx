442066d7d96df0f3ad157103e0dc25b41628b687
Ahmed Kaddoura <32470375+ahmedkadd@users.noreply.github.com>
2019-08-08 01:20:48 -0700

    three.js part done

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -10,9 +10,9 @@ import { Unicorn } from "@rahoi/ch3ss_logic/dist/Unicorn";
 import { Pawn } from "@rahoi/ch3ss_logic/dist/Pawn";
 import { Queen } from "@rahoi/ch3ss_logic/dist/Queen";
 import { BLACK, WHITE } from "@rahoi/ch3ss_logic/dist/constants";
-//import { Position } from '@rahoi/ch3ss_logic';
+import { Position } from '@rahoi/ch3ss_logic';
 
-const whiteMaterial = new THREE.MeshLambertMaterial({color: 0xd3d3d3});
+const whiteMaterial = new THREE.MeshLambertMaterial({color: 0xffffff});
 const blackMaterial = new THREE.MeshLambertMaterial({color: 0x000000});
 
 const pawnGeometry = new THREE.SphereGeometry(0.3, 16, 12);
@@ -23,40 +23,51 @@ const unicornGeometry = new THREE.IcosahedronGeometry(0.3);
 const kingGeometry = new THREE.TetrahedronGeometry(0.5);
 const queenGeometry = new THREE.ConeGeometry(0.25, 0.75, 16);
 
-const boardMaterial = new THREE.MeshBasicMaterial({color: 0xf5f5f5, transparent: true, opacity: 0.05, wireframe: true});
-const boardGeometry = new THREE.BoxGeometry(1, 1, 1);
-//const edges = new THREE.LineSegments(boardGeometry, boardMaterial);
+const boardMaterial = new THREE.MeshLambertMaterial({color: 0xfdfdfd, transparent: true, opacity: 0.05});const boardGeometry = new THREE.BoxGeometry(1, 1, 1);
 const boardDimension = 5;
 const board = new THREE.Group();
 
 let SELECTED_PIECE = null;
 let VALID_SPACES = null;
 let SELECTED_SPACE = null;
-let SPACE_SELECTION_MODE = true;
+let PIECE_SELECTION_MODE = true;
 
 function getGamePositionFromObject(object) {
     let position_3D = object.position;
-    let x = (position_3D.x + 3).toString();
-    let y = (position_3D.y + 3).toString();
-    let z = (position_3D.z + 3).toString();
+    let gamePosition;
+    let x = position_3D.x + 3;
+    let y = position_3D.y + 3;
+    let z = position_3D.z + 3;
 
-    return currentGame.getPositionFromString(x + y + z);
+    let positionString = x.toString() + y.toString() + z.toString();
+    
+    gamePosition = currentGame.getPositionFromString(positionString);
+     
+    return gamePosition;
 }
 
 function setValidSpaces(piece) {
     let validSpaces = [];
-    let validPositions = currentGame.getPossibleMovesForPieceAtSpace(getGamePositionFromObject(SELECTED_PIECE));
+    let piecePosition = getGamePositionFromObject(piece);
+    let validPositions = currentGame.getPossibleMovesForPieceAtSpace(piecePosition);
+    console.log(validPositions + " LENGTH:" + validPositions.length);
+    let position;
+    let space;
 
     for (let i = 0; i < validPositions.length; i++) {
-        let position = validPositions[i];
+        position = validPositions[i].getPostionString();
         for (let j = 0; j < board.children.length; j++) {
-            let space = board.children[j];
-            if (space.userData.x == position.getX() && space.userData.y == position.getY() && space.userData.z == position.getZ()) {
+            space = board.children[j];
+            let spaceGamePosition = getGamePositionFromObject(space).getPostionString();
+            if (spaceGamePosition == position) {
+                console.log("FOUND A MATCH!");
                 validSpaces.push(space);
             }
-            break;
         }
     }
+
+    VALID_SPACES = validSpaces;
+    console.log("VALID_SPACES:" + VALID_SPACES);
 }
 
 function isValidSpace(space) {
@@ -84,15 +95,17 @@ function highlightValidSpaces() {
     if (VALID_SPACES != null) {
         for (let i = 0; i < VALID_SPACES.length; i++) {
             let space = VALID_SPACES[i];
-            space.material.color.setHex(0x000000);
+            space.material.color.setHex(0xff0000);
         }
     }
 }
 
-function unhighlightValidSpaces() {
-    for (let i = 0; i < VALID_SPACES.length; i++) {
-        let space = VALID_SPACES[i];
-        space.material.color.setHex(boardMaterial.color);
+function unhighlightSpaces() {
+    if (VALID_SPACES != null) {
+        for (let i = 0; i < VALID_SPACES.length; i++) {
+            let space = VALID_SPACES[i];
+            space.material.color.setHex(0xfdfdfd);
+        }
     }
 }
 
@@ -164,7 +177,7 @@ export default class LiveGame extends Component {
         const OrbitControls = require('three-orbitcontrols');
         let controls = new OrbitControls(camera, renderer.domElement);
 
-        const light = new THREE.AmbientLight(0xffffff);
+        const light = new THREE.AmbientLight(0xd3d3d3);
         scene.add(light);
 
         for (let x = 0; x < boardDimension; x++) {
@@ -232,15 +245,14 @@ export default class LiveGame extends Component {
         }
 
         function onMouseDown( event ) {
-            console.log("Mouse Click");
 	        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
             mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
             let ray = new THREE.Raycaster();
             ray.setFromCamera(mouse, camera);
             let intersects = ray.intersectObjects(scene.children, true);
 
-            if (SPACE_SELECTION_MODE) {
-                console.log("PIECE SELECTION MODE");
+            if (PIECE_SELECTION_MODE) {
+                unhighlightSpaces();
                 let aPieceWasSelected = false;
                 let intersection = -1;
                 if (intersects.length > 0) {
@@ -255,7 +267,6 @@ export default class LiveGame extends Component {
                 if (aPieceWasSelected) {
                     console.log("PIECE SELECTED");
                     if (SELECTED_PIECE !== null && SELECTED_PIECE !== intersects[intersection].object) {
-                        
                         if (SELECTED_PIECE.userData.color === "white") {
                             SELECTED_PIECE.material.color.setHex(0xd3d3d3);
                         }
@@ -266,16 +277,16 @@ export default class LiveGame extends Component {
                     }
             
                     SELECTED_PIECE = intersects[intersection].object;
-                    intersects[intersection].object.material.color.setHex(0xff0000);
+                    SELECTED_PIECE.material.color.setHex(0xff0000);
 
                     console.log("COLOR SET!");
 
                     setValidSpaces(SELECTED_PIECE);
                     highlightValidSpaces();
-                    SELECTED_SPACE = null;
-                    SPACE_SELECTION_MODE = false;
+                    //SELECTED_SPACE = null;
+                    //PIECE_SELECTION_MODE = false;
                 }
-            } else {
+            } /*else {
                 console.log("SPACE SELECTION MODE");
                 // FIX BUG HERE SO CAN CLICK MULTIPLE TIMES EVEN IF MISS CORRECT SPACE
 
@@ -301,7 +312,7 @@ export default class LiveGame extends Component {
                     SELECTED_PIECE = null;
                     SPACE_SELECTION_MODE = true;
                 }
-            }
+            }*/
         }
     }
 
